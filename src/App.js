4557e28583a9150859799a34fbed4259938718e8
Jason Beedle <jason_beedle@hotmail.com>
2020-03-21 12:56:36 +0000

    appjs page navigation created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from ',.pages/AboutPage';
+import DonatePage from './pages/donatePage';
+import NotFoundPage from './pages/NotfoundPage';
+import NavBar from './NavBar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <NavBar />
+                    <div id="page-body">
+                        <Switch>
+                            <Route path="/" component={HomePage} exact>
+                            <Route path="/about" component={AboutPage} exact></Route>    
+                            <Route path="/DonatePage" component={DonatePage} exact></Route>
+                            <Route component={NotFoundPage} />
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App; 
